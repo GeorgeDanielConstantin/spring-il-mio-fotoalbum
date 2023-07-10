6586c919c1e5c5a0c4ea2a3e2836e56d05afffaf
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-07-10 16:06:37 +0200

    Add frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import PhotoList from "./components/PhotoList.vue";
+
+export default {
+  components: {
+    PhotoList,
+  },
+};
+</script>
+
+<template>
+  <PhotoList />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
